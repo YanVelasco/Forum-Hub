f9b3c1843df87cc945eae0d500cc3e67a9ba1fd8
Yan Velasco <yancarlosduartevelasco@gmail.com>
2025-05-14 15:29:02 -0300

    Add security filter chain configuration for stateless session management

--- src/main/java/br/com/forum_hub/infra/seguraca/ConfiguracoesDeSeguranca.java
+++ src/main/java/br/com/forum_hub/infra/seguraca/ConfiguracoesDeSeguranca.java
@@ -2,13 +2,28 @@ package br.com.forum_hub.infra.seguraca;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracoesDeSeguranca {
+
+    @Bean
+    public SecurityFilterChain filtrosDeSeguranca(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .sessionManagement(sm -> sm.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS
+                ))
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
